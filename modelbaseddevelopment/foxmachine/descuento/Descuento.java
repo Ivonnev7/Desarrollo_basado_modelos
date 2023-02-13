2da414ac6ed699fb7fa743b89f3c86f0624e07dd
Ivonnev7 <105189282+Ivonnev7@users.noreply.github.com>
2023-02-13 02:04:44 -0600

    Delete modelbaseddevelopment directory

--- modelbaseddevelopment/foxmachine/descuento/Descuento.java
+++
@@ -1,8 +0,0 @@
-package modelbaseddevelopment.foxmachine.descuento;
-
-public interface Descuento {
-
-
-    //Interfaz de superclase que define el método Descuento 
-    double discount(double precio);
-}
